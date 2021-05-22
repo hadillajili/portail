b0c85379f2e79f8d113bdd8a9fbb13c8370ae5c6
Hadil Lajili <300057135@fvtc.edu>
2021-05-23 00:20:16 +0200

    first com

---
+++ src/main/java/com/issat/portail/config/SecurityConfig.java
@@ -0,0 +1,4 @@
+package com.issat.portail.config;
+
+public class SecurityConfig {
+}
---
+++ src/test/java/com/issat/portail/PortailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.issat.portail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
