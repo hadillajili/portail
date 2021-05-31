a8a226d2787065af2263d158f11bc02befd89065
Hadil Lajili <300057135@fvtc.edu>
2021-05-31 15:19:58 +0200

    get all posts by userId

--- src/main/java/com/issat/portail/controller/PostController.java
+++ src/main/java/com/issat/portail/controller/PostController.java
@@ -2,15 +2,13 @@ package com.issat.portail.controller;
 
 import com.issat.portail.dto.UserPostDTO;
 import com.issat.portail.entity.Post;
-import com.issat.portail.entity.User;
 import com.issat.portail.exception.BadRequest;
 import com.issat.portail.service.PostServices;
-import com.issat.portail.service.UserServices;
-import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/post")
@@ -23,5 +21,9 @@ public class PostController {
     @PostMapping("")
     public Post create(@Valid @RequestBody UserPostDTO post) throws BadRequest {
         return postServices.create(post);
+    }
+    @GetMapping("/{id}")
+    public List<Post> getAllById(@PathVariable String id) throws BadRequest {
+        return postServices.getAllPosts(id);
     }
 }
